f9980d95d2572e7764eea83ae23299b2cec2214a
Robert <osipovitchr@gmail.com>
2022-05-09 12:16:43 -0400

    added extra product details + completed extra EJS views for those products

--- controllers/404.js
+++ controllers/404.js
@@ -1,4 +1,4 @@
 
 exports.get404 = (req, res, next) => {
-    res.status(404).render('page-not-found', {docTitle: '404'})
+    res.status(404).render('page-not-found', {docTitle: '404', path: '/404'})
 }
